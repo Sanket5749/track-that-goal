0c0399c23f3b68c97482cc71f06fc1ebeea1478f
Sanket Patil <sanketpatil574918@gmail.com>
2025-04-20 16:15:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "http://track-that-goal.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
